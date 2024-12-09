c1575c2cc8ed3c1a4f333940101cfb7de67b23d1
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-12-09 23:19:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Working Fine With 1st deployment" });
+});
+
+app.listen(3000, () => {
+  console.log("Server Running at 3000");
+});
